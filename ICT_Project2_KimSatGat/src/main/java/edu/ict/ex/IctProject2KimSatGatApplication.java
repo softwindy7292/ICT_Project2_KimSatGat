5da247397caedc3930eb6e243a47a7ef3f0a86a4
WIN-OPM3TB4AK0M\ICT02_19 <asdf@gmail.com>
2024-10-01 17:40:46 +0900

    김삿갓 프로젝트 틀

---
+++ ICT_Project2_KimSatGat/src/main/java/edu/ict/ex/IctProject2KimSatGatApplication.java
@@ -0,0 +1,13 @@
+package edu.ict.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IctProject2KimSatGatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IctProject2KimSatGatApplication.class, args);
+	}
+
+}
